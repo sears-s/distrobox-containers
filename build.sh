4a764b77104b572c9b643db800518f46377a2083
Sears Schulz <36250748+sears-s@users.noreply.github.com>
2023-04-18 18:37:34 -0500

    Add Kali container and home links

--- build.sh
+++ build.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 
+# Must have 1 argument
 if [ $# -eq 0 ]; then
     >&2 echo "Provide container directory name as argument"
     exit 1
 fi
-
+SCRIPT=$(realpath "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
 CONTAINER_NAME=$1-distrobox
-CONTAINER_DIR=$(dirname "$0")/$1
-echo $CONTAINER_NAME
-echo $CONTAINER_DIR
+CONTAINER_DIR=$SCRIPT_DIR/$1
+
+# Stop old container
 distrobox stop -r -Y $1
 distrobox rm -r -Y $1
-sudo podman build --pull=newer --no-cache -t $CONTAINER_NAME $CONTAINER_DIR 
+
+# Build the image
+sudo podman build --pull=newer --no-cache -t $CONTAINER_NAME \
+	--build-arg USERNAME=$(whoami) \
+	--build-arg UID=$(id -u) \
+	--build-arg GID=$(id -g) \
+	$CONTAINER_DIR || exit 1 
 sudo podman image prune -f
-distrobox create -n $1 -i $CONTAINER_NAME -r -Y --init-hooks "chsh -s /bin/fish sears"
+
+# Setup the home directory
+mkdir -p $CONTAINER/home
+mapfile -t LINKS < $SCRIPT_DIR/home_links.txt
+for LINK in ${LINKS[@]}; do
+	SRC_DIR=~/$LINK
+	DST_DIR=$CONTAINER_DIR/home/$LINK
+	rm -rf $DST_DIR
+	mkdir -p $(dirname "$DST_DIR")
+	ln -s $SRC_DIR $DST_DIR || exit 1
+done
+
+# Start the container
+SHELL=/bin/fish distrobox create -r -Y -n $1 -i $CONTAINER_NAME -H $CONTAINER_DIR/home
 distrobox enter -r $1
